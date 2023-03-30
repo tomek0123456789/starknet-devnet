56e770e900bfd7e2355221e6a2a400e8af45143d
mikiw <mikiw.pp@gmail.com>
2023-03-30 13:49:29 +0200

    Update test_abort_blocks_after.py
    [skip ci]

--- test/test_abort_blocks_after.py
+++ test/test_abort_blocks_after.py
@@ -43,15 +43,13 @@ def test_abort_not_existing_block():
 def test_abort_single_block_single_transaction():
     """Test abort of single block and single transaction."""
 
-    # Contract deploy block should be accepted on L2 and
-    # transaction should be accepted on L2
+    # Block and transaction should be accepted on L2
     contract_deploy_info = deploy(CONTRACT_PATH, inputs=["0"])
     contract_deploy_block = get_block(parse=True)
     assert contract_deploy_block["status"] == "ACCEPTED_ON_L2"
     assert_tx_status(contract_deploy_info["tx_hash"], "ACCEPTED_ON_L2")
 
-    # After abort blocks call block should be aborted and
-    # transaction should be rejected
+    # Blocks should be aborted and transactions should be rejected
     response = abort_blocks(contract_deploy_block["block_hash"])
     contract_deploy_block_after_abort = get_block(parse=True)
     assert response.status_code == 200
@@ -67,15 +65,13 @@ def test_abort_single_block_single_transaction():
 def test_abort_many_blocks_many_transactions():
     """Test abort of many blocks and many transactions."""
 
-    # Contract deploy block should be accepted on L2 and
-    # transaction should be accepted on L2
+    # Block and transaction should be accepted on L2
     contract_deploy_info = deploy(CONTRACT_PATH, inputs=["0"])
     contract_deploy_block = get_block(parse=True)
     assert contract_deploy_block["status"] == "ACCEPTED_ON_L2"
     assert_tx_status(contract_deploy_info["tx_hash"], "ACCEPTED_ON_L2")
 
-    # Increase balance block should be accepted on L2 and
-    # transaction should be accepted on L2
+    # Block and transaction should be accepted on L2
     invoke_tx_hash = invoke(
         calls=[(contract_deploy_info["address"], "increase_balance", [10, 20])],
         account_address=PREDEPLOYED_ACCOUNT_ADDRESS,
@@ -85,8 +81,7 @@ def test_abort_many_blocks_many_transactions():
     assert invoke_block["status"] == "ACCEPTED_ON_L2"
     assert_transaction(invoke_tx_hash, "ACCEPTED_ON_L2")
 
-    # After abort blocks call blocks should be aborted and
-    # transactions should be rejected
+    # Blocks should be aborted and transactions should be rejected
     response = abort_blocks(contract_deploy_block["block_hash"])
     assert response.status_code == 200
     assert response.json()["aborted"] == [
@@ -104,7 +99,7 @@ def test_abort_many_blocks_many_transactions():
     rpc_response = rpc_call("starknet_getBlockWithTxs", params={"block_id": "latest"})
     assert rpc_response["result"]["status"] == "REJECTED"
 
-    # Newly deployed contract after abort should be accepted on L2
+    # Block and transaction should be accepted on L2
     contract_deploy_info = deploy(CONTRACT_PATH, inputs=["0"])
     contract_deploy_block = get_block(parse=True)
     assert contract_deploy_block["status"] == "ACCEPTED_ON_L2"
